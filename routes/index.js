5173647725f00bca2cdb5e3b1d29d3225c02883c
sam202020 <shalomlandsman@gmail.com>
2020-10-29 17:55:51 -0500

    fix payment amount

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -11,15 +10,13 @@ router.post("/process-payment", async (req, res) => {
   console.log(req.body);
   const request_params = req.body;
 
-  // length of idempotency_key should be less than 45
   const idempotency_key = uuidv4();
 
-  // Charge the customer's card
   const payments_api = new squareConnect.PaymentsApi();
   const request_body = {
     source_id: request_params.nonce,
     amount_money: {
-      amount: 100, // $1.00 charge
+      amount: request_params.amount * 1500,
       currency: "USD",
     },
     idempotency_key: idempotency_key,
