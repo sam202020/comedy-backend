202f6d91e0736d222aa67f795f19ef4d129c3d22
sam202020 <shalomlandsman@gmail.com>
2020-10-30 17:25:20 -0500

    add email tickets

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const { v4: uuidv4 } = require("uuid");
+const sgMail = require("@sendgrid/mail");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const fs = require("fs");
+
+pathToAttachment = `${__dirname}/ticket.jpg`;
+attachment = fs.readFileSync(pathToAttachment).toString("base64");
+
+const sendMail = async (email) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: "zev@prescottcomedyclub.com",
+    subject: "Your ticket to Prescott Comedy Club",
+    text: "We look forward to seeing you!",
+    html: "<p>We look forward to seeing you!</p>",
+    attachments: [
+    {
+      content: attachment,
+      filename: "ticket.jpg",
+      type: "image/jpeg",
+      disposition: "attachment",
+    },
+  ],
+  };
+  try {
+    return await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body)
+      return error.response.body;
+    } else {
+      return error;
+    }
+  }
+};
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 router.post("/process-payment", async (req, res) => {
-  console.log(req.body);
   const request_params = req.body;
 
   const idempotency_key = uuidv4();
@@ -22,14 +58,16 @@ router.post("/process-payment", async (req, res) => {
     idempotency_key: idempotency_key,
   };
 
+  const email = request_params.email;
+  console.log(email);
+
   try {
     const response = await payments_api.createPayment(request_body);
+    await sendMail(email);
     res.status(200).json(response);
   } catch (error) {
-    
     res.status(500).json(error);
   }
 });
-
 
 module.exports = router;
