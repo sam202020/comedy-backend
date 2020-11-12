fe962978a3f0c8e4956f4c2a4e78d7eddb562360
sam202020 <shalomlandsman@gmail.com>
2020-11-12 14:23:51 -0600

    add thanks, bcc

--- routes/index.js
+++ routes/index.js
@@ -20,11 +20,12 @@ const sendMail = async (email, amount) => {
   }
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: [email, "zevengelson7@gmail.com", "samlandsman20@gmail.com"],
+    to: email,
+    bcc: ["zevengelson7@gmail.com", "samlandsman20@gmail.com"],
     from: "tickets@prescottcomedyclub.com",
-    subject: "Your ticket to Prescott Comedy Club",
+    subject: "Your tickets to Prescott Comedy Club",
     text: "We look forward to seeing you!",
-    html: "<p>We look forward to seeing you!</p>",
+    html: "<p>Thank you for your support. We look forward to seeing you!</p>",
     attachments: attachments,
   };
   try {
